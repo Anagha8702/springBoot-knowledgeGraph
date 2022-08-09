8f624a47e77f91b797fba80e96fb045b5642e58f
Anagha Anand <89851764+Anagha8702@users.noreply.github.com>
2022-08-09 22:08:20 +0530

    Add files via upload

--- src/main/java/reshamandi/knowledgeGraph/Query1_2Controller.java
+++ src/main/java/reshamandi/knowledgeGraph/Query1_2Controller.java
@@ -9,7 +9,6 @@ import java.util.Arrays;
 
 @Controller
 public class Query1_2Controller {
-
     @GetMapping("/greeting")
     public String greetingForm(Model model) {
         model.addAttribute("q12", new Query1_2());
@@ -24,11 +23,16 @@ public class Query1_2Controller {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+       // String data[][] = {{"Type","Blore","Chennai","Hyderabad"},{"Accessore","23","433","134"},{"Saree","334","34","22"}};
+        String param2[] = new String[data[0].length];
+        for(int i=1;i<data[0].length;i++) param2[i-1]=data[0][i];
         model.addAttribute("q12", q12);
         model.addAttribute("data", data);
         model.addAttribute("tableDisplay", 1);
-
+        model.addAttribute("data_row", data.length);
+        model.addAttribute("data_col", data[0].length);
+        model.addAttribute("param2", param2);
         return "index";
     }
-
 }
+
