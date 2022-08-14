0a710ac40c6b7a6e33a20d2ade9ee63e64d88491
adnes-tricky <adarshvkalathur17@gmail.com>
2022-08-14 13:42:58 +0530

    Adarsh: New file added called Retailer.java with getters and setters ps:namimg convention follow retailer_excel_sheet

---
+++ src/main/java/reshamandi/knowledgeGraph/Retailer.java
@@ -0,0 +1,284 @@
+package reshamandi.knowledgeGraph;
+
+public class Retailer {
+    private String id;
+    private String created_by;
+    private String created_date;
+    private String category;
+    private String cost_price;
+    private String quantity;
+    private String selling_price;
+    private String status;
+    private String type;
+    private String warehouseid;
+    private String sold_quantity;
+    private String sku_listing_status;
+    private String landing_price;
+    private String uom;
+    private String gst_amount;
+    private String gst_percentage;
+    private String logistics_amount;
+    private String weave;
+    private String cst;
+    private String igst;
+    private String total_amount;
+    private String total_pre_tax_price;
+    private String sku_count;
+    private String sku_total_quantity;
+    private String state;
+    private String retailer_id;
+    private String discount;
+    private String gross_amount;
+    private String discount_amount;
+    private String business_type;
+    private String transaction_id;
+
+    //1. id
+    public String getid() {
+        return id;
+    }
+    public void setid(String id){
+        this.id = id;
+    }
+
+    //2.created_by
+    public String getcreated_by() {
+        return created_by;
+    }
+    public void setcreated_by(String created_by){
+        this.created_by = created_by;
+    }
+
+    //3. created_date
+    public String getcreated_date() {
+        return created_date;
+    }
+    public void setcreated_date(String created_date){
+        this.created_date = created_date;
+    }
+
+    //4.category
+    public String getcategory() {
+        return category;
+    }
+    public void setcategory(String category){
+        this.category = category;
+    }
+
+    //5.cost_price
+    public String getcost_price() {
+        return cost_price;
+    }
+    public void setcost_price(String cost_price){
+        this.cost_price = cost_price;
+    }
+    
+    //6. quantity
+    public String getquantity() {
+        return quantity;
+    }
+    public void setquantity(String quantity){
+        this.quantity = quantity;
+    }
+
+    //7. selling_price
+    public String getselling_price() {
+        return selling_price;
+    }
+    public void setselling_price(String selling_price){
+        this.selling_price = selling_price;
+    }
+
+    //8.status
+    public String getstatus() {
+        return status;
+    }
+    public void setstatus(String status){
+        this.status = status;
+    }
+
+    //9. type
+    public String gettype() {
+        return type;
+    }
+    public void settype(String type){
+        this.type = type;
+    }
+
+    //10. warehouseid
+    public String getwarehouseid() {
+        return warehouseid;
+    }
+    public void setwarehouseid(String warehouseid){
+        this.warehouseid = warehouseid;
+    }
+
+    //11. sold_quantity
+    public String getsold_quantity() {
+        return sold_quantity;
+    }
+    public void setsold_quantity(String sold_quantity){
+        this.sold_quantity = sold_quantity;
+    }
+
+    //12. sku_listing_status
+    public String getsku_listing_status() {
+        return sku_listing_status;
+    }
+    public void setsku_listing_status(String sku_listing_status){
+        this.sku_listing_status = sku_listing_status;
+    }
+
+    //13. landing_price
+    public String getlanding_price() {
+        return landing_price;
+    }
+    public void setlanding_price(String landing_price){
+        this.landing_price = landing_price;
+    }
+
+    //14. uom
+    public String getuom() {
+        return uom;
+    }
+    public void setuom(String uom){
+        this.uom = uom;
+    }
+    
+    //15. gst_amount
+    public String getgst_amount() {
+        return gst_amount;
+    }
+    public void setgst_amount(String gst_amount){
+        this.gst_amount = gst_amount;
+    }
+
+    //16. gst_percentage
+    public String getgst_percentage() {
+        return gst_percentage;
+    }
+    public void setgst_percentage(String gst_percentage){
+        this.gst_percentage = gst_percentage;
+    }
+
+    //17. logistics_amount
+    public String getlogistics_amount() {
+        return logistics_amount;
+    }
+    public void setlogistics_amount(String logistics_amount){
+        this.logistics_amount = logistics_amount;
+    }
+
+    //18. weave
+    public String getweave() {
+        return weave;
+    }
+    public void setweave(String weave){
+        this.weave = weave;
+    }
+
+    //19. cst
+    public String getcst() {
+        return cst;
+    }
+    public void setcst(String cst){
+        this.cst = cst;
+    }
+
+    //20. igst
+    public String getigst() {
+        return igst;
+    }
+    public void setigst(String igst){
+        this.igst = igst;
+    }
+
+    //21. total_amount
+    public String gettotal_amount() {
+        return total_amount;
+    }
+    public void settotal_amount(String total_amount){
+        this.total_amount = total_amount;
+    }
+
+    //22. total_pre_tax_price
+    public String gettotal_pre_tax_price() {
+        return total_pre_tax_price;
+    }
+    public void settotal_pre_tax_price(String total_pre_tax_price){
+        this.total_pre_tax_price = total_pre_tax_price;
+    }
+
+    //23. sku_count
+    public String getsku_count() {
+        return sku_count;
+    }
+    public void setsku_count(String sku_count){
+        this.sku_count = sku_count;
+    }
+
+    //24. sku_total_quantity
+    public String getsku_total_quantity() {
+        return sku_total_quantity;
+    }
+    public void setsku_total_quantity(String sku_total_quantity){
+        this.sku_total_quantity = sku_total_quantity;
+    }
+
+    //25. state
+    public String getstate() {
+        return state;
+    }
+    public void setstate(String state){
+        this.state = state;
+    }
+
+    //26. retailer_id
+    public String getretailer_id() {
+        return retailer_id;
+    }
+    public void setretailer_id(String retailer_id){
+        this.retailer_id = retailer_id;
+    }
+
+    //27. discount
+    public String getdiscount() {
+        return discount;
+    }
+    public void setdiscount(String discount){
+        this.discount = discount;
+    }
+
+    //28. gross_amount
+    public String getgross_amount() {
+        return gross_amount;
+    }
+    public void setgross_amount(String gross_amount){
+        this.gross_amount = gross_amount;
+    }
+
+    //29. discount_amount
+    public String getdiscount_amount() {
+        return discount_amount;
+    }
+    public void setdiscount_amount(String discount_amount){
+        this.discount_amount = discount_amount;
+    }
+
+    //30. business_type
+    public String getbusiness_type() {
+        return business_type;
+    }
+    public void setbusiness_type(String business_type){
+        this.business_type = business_type;
+    }
+
+    //31. transaction_id
+    public String gettransaction_id() {
+        return transaction_id;
+    }
+    public void settransaction_id(String transaction_id){
+        this.transaction_id = transaction_id;
+    }
+
+}
